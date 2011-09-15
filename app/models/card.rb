0d2077ce2878a8e17f947c3e56a69cc3caf67e20
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2011-09-14 19:41:42 -0700

    Added slides as the index page for all things with media.

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,7 @@ class Card < ActiveRecord::Base
   belongs_to :institution
 
   has_many :media_items
+  has_one :primary_media_item, :as => :attachable, :class_name => 'MediaItem', :conditions => {:display_order => 1}
 
   acts_as_relatable :artefact
   
