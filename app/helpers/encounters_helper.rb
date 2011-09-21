07b5e82341969f644e70b162bd433400431fa0a7
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2011-09-21 10:03:52 -0700

    Made a toggle button to switch between encounters with the same artefact.

--- app/helpers/encounters_helper.rb
+++ app/helpers/encounters_helper.rb
@@ -1,8 +1,10 @@
 module EncountersHelper
   def encounter_show_menu_bar
-    MenuBar.new(self) do |menu|
-      for encounter in @encounter.other_encounters
-        menu.add_menu_bar_item(link_to image_tag('flip.png') + " #{encounter.encounter_type.titleize}'s Encounter", encounter)
+    MenuBar.new(self, :toggle => true) do |menu|
+      menu.group do |group|
+        for encounter in @encounter.artefact.encounters
+          group.menu_bar_item(link_to encounter.encounter_type.titleize, encounter).selected(@encounter == encounter)
+        end        
       end
     end
   end
