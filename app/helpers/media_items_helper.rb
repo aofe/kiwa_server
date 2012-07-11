bea0e45fce6f7ac8f4a4d372fc0c37ca8205cfcf
Administrator <sysad@maa-jmd6089.arch.private.cam.ac.uk>
2012-07-11 15:58:49 +0100

    Added image zoom to labels section

--- app/helpers/media_items_helper.rb
+++ app/helpers/media_items_helper.rb
@@ -5,7 +5,7 @@ module MediaItemsHelper
     return '' unless media_item
     
     # Set up default options, this allows us the options of specifying a size and/or a different object to link to than the larger image
-    options.reverse_merge! :size => 100, :link_to => media_item.source_url
+    options.reverse_merge! :size => 100, :link_to => media_item.source_url + "kiwaserver/"
     
     link_to_if options[:link_to], image_tag(media_item.thumbnail_url(options[:size])), options[:link_to], :class => 'media_thumbnail' do
       content_tag :span, image_tag(media_item.thumbnail_url(options[:size])), :class => 'media_thumbnail'
--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,9 +1,9 @@
-class MediaItem < ActiveRecord::Base
+	class MediaItem < ActiveRecord::Base
   belongs_to :attachable, :polymorphic => true
   
   validates_uniqueness_of :display_order, :scope => [:attachable_id, :attachable_type]
   
 	def thumbnail_url(height)
-	  self.source_url + "/#{height}/"
+	  self.source_url + "kiwaserver/#{height}/"
 	end
 end
