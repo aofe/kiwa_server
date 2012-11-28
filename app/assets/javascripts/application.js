95691c0767f971b1e333ff797fd0eb2066148950
Nicholas Jakobsen <nicholas.jakobsen@telus.net>
2012-11-27 22:14:50 -0800

    Not loading popup gallery if on a mobile device. Closes #27.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,24 @@ $(document).on('ajax:error', '.modal', function(){
   element.attr('data-state', 'error')
   setTimeout(function(){element.removeAttr('data-state')}, 1500)
 })
+
+isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
